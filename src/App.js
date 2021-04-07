38e05b9f4aef0c30ec1a76a94e61b2c242ddbd20
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-04-06 20:58:13 -0400

    API call success

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import BrainStormComp from "./brainstorm"
+
 
 function App() {
   return (
     <div className="App">
       <h1>CAPSTONE 2</h1>
+      <BrainStormComp />
     </div>
   );
 }
