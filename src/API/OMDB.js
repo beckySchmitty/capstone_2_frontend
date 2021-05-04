8ad6f96d6ac4d5627e21cf894ff427188df51caa
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-05-04 17:49:22 -0400

    deploy fix

--- src/API/OMDB.js
+++ src/API/OMDB.js
@@ -4,10 +4,10 @@ import axios from "axios";
 
 
 const API_KEY = process.env.REACT_APP_OMDB_API_KEY
-const BASE_URL = `https://www.omdbapi.com/?apikey=${API_KEY}`
+const URL = `https://www.omdbapi.com/?apikey=${API_KEY}`
 
 async function getMovie(imdb_id) {
-    let resp = await axios.get(`${BASE_URL}&i=tt0${imdb_id}`)
+    let resp = await axios.get(`${URL}&i=tt0${imdb_id}`)
     return resp.data;
 }
 
